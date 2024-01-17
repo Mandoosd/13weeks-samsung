8591607c7d1cadf8d733a9c6289ebb25f55217a0
Mandoosd <148739244+Mandoosd@users.noreply.github.com>
2024-01-18 08:32:21 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -3,17 +3,15 @@
 $(function () {
 
     var swiper = new Swiper(".mySwiper", {
-        spaceBetween: 30,
         centeredSlides: true,
         loop: true,
         speed: 1000,
         autoplay: {
-            delay: 3000,
+            delay: 2000,
             disableOnInteraction: false,
         },
     });
     var swiper = new Swiper(".sub_swiper", {
-        spaceBetween: 30,
         centeredSlides: true,
         loop: true,
         speed: 1000,
