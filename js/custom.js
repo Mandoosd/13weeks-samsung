a8d04c648617e89d7ac73ac483db60d825e3a466
Mandoosd <148739244+Mandoosd@users.noreply.github.com>
2024-01-18 18:12:02 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -12,13 +12,35 @@ $(function () {
         },
     });
     var swiper = new Swiper(".sub_swiper", {
-        centeredSlides: true,
-        loop: true,
+        slidesPerView: 4,
+        spaceBetween: 15,
         speed: 1000,
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
+
+
+
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
         },
+
+
+
+    });
+    var swiper = new Swiper(".txt_swiper", {
+        slidesPerView: 3,
+
+        spaceBetween: 15,
+        speed: 1000,
+
+
+
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+
+
+
     });
     $(document).ready(function () {
         AOS.init();
